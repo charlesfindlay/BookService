dce0a06f1a2dfba1f72cca3fde7a6507d61f327e
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-06 13:41:53 -0400

    added models and controllers

---
+++ BookService/Models/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace BookService.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
