a98a3b8fdebd68ca962c4e4469c807f44ad74eb4
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-08 10:14:59 -0400

    Changed GET controllers to use DTO

--- BookService/Models/DTO.cs
+++ BookService/Models/DTO.cs
@@ -5,7 +5,20 @@ using System.Web;
 
 namespace BookService.Models
 {
-    public class DTO
+    public class BookDTO
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+    }
+
+    public class BookDetailDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+        public string AuthorName { get; set; }
+        public string Genre { get; set; }
     }
 }
