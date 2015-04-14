def9ede4fcfd657321eee32141a89758ed615ad4
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-14 10:50:20 -0400

    Added seed for online database and publish

--- BookService/Global.asax.cs
+++ BookService/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookService.App_Start;
+using BookService.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,12 +9,14 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
+
 namespace BookService
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            DbConfig.SeedIfNotEmpty(new BookServiceContext());
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
